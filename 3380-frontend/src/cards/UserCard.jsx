9f622b6b55d392355e0f18e90a692fbf8739b18c
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2022-04-04 15:51:01 -0500

    back to basic login format

--- 3380-frontend/src/cards/UserCard.jsx
+++ 3380-frontend/src/cards/UserCard.jsx
@@ -4,74 +4,28 @@ import { Button } from 'primereact/button';
 // Uncomment these lines to use/modify user object globally
 
 // import { useGlobal } from '../util/GlobalContext';
+const handleSubmit = (event) => {
+  event.preventDefault();
+  submit();
+};
+
 
 function UserCard() {
-  // const { user, setUser } = useGlobal();
-    state = {
-      emailAddress: '',
-      //password: '',
-      //errors: [],
-    };
-
-    //THIS.STATE SET
-  
-    const {
-      emailAddress,
-      //password,
-      //errors,
-    } = this.state;
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      submit();
-    };
-
-    const handleCancel = (event) => {
-      event.preventDefault();
-      cancel();
-    };
-
   return (
     <div className="card">
-      <div className="h600">Sign In:</div>
+      <div className="h600">Sign In</div>
+      <div className="h400">
+        <label>Email:</label>
+        <input></input>
+        <label>Password:</label>
+        <input></input>
+      </div>
       <div className="spacer" />
       <div className="p400">
-          <form 
-            cancel={this.cancel}
-            //errors={errors}
-            submit={this.submit}
-            submitButtonText="Sign In"
-            elements={() => (
-              <React.Fragment>
-                <input 
-                  id="emailAddress" 
-                  name="emailAddress" 
-                  type="text"
-                  value={emailAddress} 
-                  onChange={this.change} 
-                  placeholder="Email Address" />
-                <Button className="p-button-rounded p-button-outlined" type="submit" onClick={handleSubmit}>Sign In</Button>
-                <Button className="p-button-rounded p-button-secondary p-button-outlined" onClick={handleCancel}>Cancel</Button>               
-              </React.Fragment>
-            )} /> 
+        <Button onClick={handleSubmit}>Sign In</Button>
       </div>
     </div>
   );
-
 }
-
-
-//change = (event) => {
-//  const name = event.target.name;
-//  const value = event.target.value;
-
-  //changes old value to new 
- // this.setState(() => {
-//    return {
-//      [name]: value
-//    };
-//  });
-//}
-
 
   export default UserCard;
