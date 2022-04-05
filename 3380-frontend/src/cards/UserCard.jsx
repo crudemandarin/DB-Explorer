deeda737ee1a302d4208f0d1bbeaf89b36c98744
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2022-04-04 19:51:40 -0500

    Update UserCard.jsx

--- 3380-frontend/src/cards/UserCard.jsx
+++ 3380-frontend/src/cards/UserCard.jsx
@@ -15,10 +15,14 @@ function UserCard() {
     <div className="card">
       <div className="h600">Sign In</div>
       <div className="h400">
-        <label>Email:</label>
-        <input></input>
-        <label>Password:</label>
-        <input></input>
+        <label>
+          Email:
+          <input/>
+        </label>
+        <label>
+          Password:
+          <input/>
+        </label>
       </div>
       <div className="spacer" />
       <div className="p400">
