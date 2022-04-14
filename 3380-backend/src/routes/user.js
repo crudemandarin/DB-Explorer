1f6bc966fb13ed9b8e153169e91b141ec4b6cda3
Anil Shanker <anilshanker2@gmail.com>
2022-04-13 20:54:53 -0500

    Implemented LOGIN endpoint

--- 3380-backend/src/routes/user.js
+++ 3380-backend/src/routes/user.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const { select } = require('../SQLManager');
 
 const router = express.Router();
+const USER_TABLE_NAME = 'User';
+const USER_TABLE_EMAIL_FIELD = 'Email';
 
-// const SQLManager = require('../SQLManager');
+const SQLManager = require('../SQLManager');
 
 /* POST /user/login */
 router.get('/login', async (req, res) => {
     console.log('POST /user/login');
-
-    return res.status(501).json({ message: 'Not implemented' });
+    const { email } = req.body;
+    if (!email) {
+        return res.status(400).json({ message: `Bad input - missing email` })
+    }
+    const where = { 'name': USER_TABLE_EMAIL_FIELD, 'value': email };
+    const user = await SQLManager.select(USER_TABLE_NAME, '*', [where]);
+    if (!user) {
+        return res.status(404).json({ message: `User doesn't exist` })
+    }
+    return res.status(200).json({ user });
 });
 
 module.exports = router;
