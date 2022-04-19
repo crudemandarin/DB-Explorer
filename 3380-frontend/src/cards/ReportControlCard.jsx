e29481e44ae37016bfaf646a6efa7992f0b93be1
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2022-04-19 01:50:38 -0500

    added the reset button to report request
    I added the reset button to the request card. I also added some commented out code for potential later use in the logic and real implementation of the submit and stuff.

--- 3380-frontend/src/cards/ReportControlCard.jsx
+++ 3380-frontend/src/cards/ReportControlCard.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { Button } from 'primereact/button';
 import { MultiSelect } from 'primereact/multiselect';
 import { Calendar } from 'primereact/calendar'
-import { Checkbox } from 'primereact/checkbox';
+// import { Checkbox } from 'primereact/checkbox';
+// import Utils from '../util/Utils';
 
 function handleSubmit() {
   // for submit button, pulls from back end 
+  // startdate = '';
+  // dateCheck();
 }
 
 // function handleChange() {
   // check boxes and such
 // }
 
-// function dateCheck() {
+function dateCheck() {
   // make sure end date > start date
-// }
+   // if (start < end){}
+
+}
 
 // need to populate multiselect for project and workpaces based on user role
 
 function ReportControlCard() {
+
+  const handleReset = () => {
+    // setTableForm(Utils.getEmptyForm(fields));
+    // setResetFlag(false);
+  };
   
   return (
     <div className="card" style={{ width: '100%' }}>
@@ -26,16 +36,7 @@ function ReportControlCard() {
       <br/>
       <form className='report-form'>
         <div>
-          Report Options:    
-          <Checkbox/>
-          <label>Basic     </label>
-          <Checkbox/>
-          <label>Enriched</label>
-        </div>
-        <div className='spacer'/>
-        <div>
           <MultiSelect placeholder='Select Workspace'/>       
-          
         </div>
 
         <div className='spacer'/>
@@ -48,12 +49,26 @@ function ReportControlCard() {
         <div>
           <Calendar className='start-date' placeholder='Start Date'/>
           -
-          <Calendar className='end-date' placeholder='End Date'/>
+          <Calendar className='end-date' placeholder='End Date' onChange={dateCheck}/>
         </div>
           
       </form>
       <div className='spacer'/>
-      <Button type='submit' onSubmit={handleSubmit}>Submit Request</Button>
+      <div>
+        <Button
+            label="Submit Request"
+            onClick={handleSubmit}
+            className="p-button-success"
+            style={{ marginRight: '10px' }}
+          />
+        <Button
+            label="Reset"
+            onClick={handleReset}
+            className="p-button-secondary"
+            style={{ marginRight: '10px' }}
+            // disabled={!resetFlag}
+          />
+      </div>
     </div>
   );
 }
