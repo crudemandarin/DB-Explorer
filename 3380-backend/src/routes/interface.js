3e0954aa5b771a9c1aaa63dc2e0459f12258f6f7
Anil-Shanker <anilshanker2@gmail.com>
2022-04-20 21:03:50 -0500

    Implemented fully-functioning update/delete endpoints

--- 3380-backend/src/routes/interface.js
+++ 3380-backend/src/routes/interface.js
@@ -95,41 +95,42 @@ router.post('/query/data', async (req, res) => {
 router.delete('/query/data', async (req, res) => {
     console.log('DELETE /interface/query/data');
 
-    const { table, fields } = req.query;
-    const fieldsObj = JSON.parse(fields);
-
-    if (!table) return res.status(400).json({ message: 'Missing `table` in body' });
+    const { table, rowParams } = req.query;
+    if (!table) return res.status(400).json({ message: 'Missing `table` in query parameters' });
+    if (!rowParams) return res.status(400).json({ message: 'Missing `rowParams` in query parameters' });
+    const rowParamsObj = JSON.parse(rowParams);
+    if (!Array.isArray(rowParamsObj)) 
+        return res.status(400).json({ message: '`rowParams` is not an array' });
+    if (rowParamsObj.length === 0) return res.status(400).json({ message: '`rowParams` is empty' });
 
     try {
-        const [results, SQL] = await SQLService.delete(table, fieldsObj);
+        const [results, SQL] = await SQLService.delete(table, rowParamsObj);
         return res.status(200).json({ results, SQL });
     } catch (err) {
-        console.error('SQLService.delete failed. err =', err);
+        console.error('SQLService.update failed. err =', err);
         if ('code' in err && 'sqlMessage' in err && 'sql' in err) {
             return res.status(400).json({ code: err.code, message: err.sqlMessage, sql: err.sql });
         }
     }
 
-    return res.status(500).json({ message: 'Delete failed' });
+    return res.status(500).json({ message: 'Failed to update data' });
 });
 
 /* PUT /interface/query/data */
 router.put('/query/data', async (req, res) => {
     console.log('PUT /interface/query/data');
 
-    const { table, fields, where } = req.body;
+    const { table, rowParams } = req.body;
 
     if (!table) return res.status(400).json({ message: 'Missing `table` in body' });
-    if (!fields) return res.status(400).json({ message: 'Missing `fields` in body' });
-    if (!Array.isArray(fields))
-        return res.status(400).json({ message: '`fields` is not an array' });
-    if (fields.length === 0) return res.status(400).json({ message: '`fields` is empty' });
-    if (!where) return res.status(400).json({ message: 'Missing `where` in body' });
-    if (!Array.isArray(where)) return res.status(400).json({ message: '`where` is not an array' });
-    if (where.length === 0) return res.status(400).json({ message: '`where` is empty' });
+    if (!rowParams) return res.status(400).json({ message: 'Missing `rowParams` in body' });
+    if (!Array.isArray(rowParams)) 
+        return res.status(400).json({ message: '`rowParams` is not an array' });
+    if (rowParams.length === 0) return res.status(400).json({ message: '`rowParams` is empty' });
+
 
     try {
-        const [results, SQL] = await SQLService.update(table, fields, where);
+        const [results, SQL] = await SQLService.update(table, rowParams);
         return res.status(200).json({ results, SQL });
     } catch (err) {
         console.error('SQLService.update failed. err =', err);
